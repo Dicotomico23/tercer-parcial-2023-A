132f3068ea24f6c2c4fa2bf24a10c50d9061fc0d
Dicotomico23 <A302486@alumnos.uaslp.mx>
2023-05-08 10:22:18 -0500

    Entrega Examen Parcial Diego López Facundo

--- src/test/java/edu/uaslp/objetos/shoppingcart/Exercise3ShoppingItemCatalog.java
+++ src/test/java/edu/uaslp/objetos/shoppingcart/Exercise3ShoppingItemCatalog.java
@@ -10,16 +10,26 @@ public class Exercise3ShoppingItemCatalog {
         ShoppingItemCatalog shoppingItemCatalog = new ShoppingItemCatalog();
         ShoppingCart shoppingCart = new ShoppingCart(shoppingItemCatalog);
         String code = "ABC";
+        int size = 0;
         //Execution
         shoppingCart.add(code);
+        size = shoppingCart.getTotalItemsCOunt();
         ShoppingItem item = shoppingItemCatalog.getItem("ABC");
         //Validation
         Assertions.assertEquals(code,item.getCode());
+        Assertions.assertEquals(1,size);
         Assertions.assertNotNull(item);
     }
-
     @Test
     public void givenANotValidCode_whenGetItem_thenNullIsReturned(){
-
+        //Initialization
+        ShoppingItemCatalog shoppingItemCatalog = new ShoppingItemCatalog();
+        ShoppingCart shoppingCart = new ShoppingCart(shoppingItemCatalog);
+        String code = "ABC";
+        //Execution
+        shoppingCart.add(code);
+        ShoppingItem item = shoppingItemCatalog.getItem("invalid_Code");
+        //Validation
+        Assertions.assertNull(item);
     }
 }
